3ae0f85acbbff8be3a3fc34fe260b7106c5487cb
frontiertsymbal <frontier.dj@gmail.com>
2015-04-08 18:56:26 +0300

    Add splashScreen

--- src/com/frontier/botChat/SplashActivity.java
+++ src/com/frontier/botChat/SplashActivity.java
@@ -1,15 +1,41 @@
 package com.frontier.botChat;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
 public class SplashActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splash);
+
+        new LoadTask().execute();
+
+
+    }
+
+    private class LoadTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
